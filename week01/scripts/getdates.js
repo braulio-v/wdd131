b397f8f524a1848e3740eae434bbf198ba85d244
Braulio Villa <bvillamiranda@byupathway.edu>
2025-09-15 15:17:01 -0500

    done2

--- week01/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-const currentYear = new Date().getFullYear();
-
-document.getElementById("currentyear").textContent = `© ${currentYear} . Braulio Villa . Ecuador`;
-
-const lastModified = document.lastModified;
-
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
