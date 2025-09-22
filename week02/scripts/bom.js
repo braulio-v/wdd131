b4e80ae89b18368bdd8fbc082268e159cf630154
Braulio Villa <bvillamiranda@byupathway.edu>
2025-09-22 17:43:35 -0500

    project finished

---
+++ week02/scripts/bom.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('arial-label', `Remove ${input.value}`);
+
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+    }   else {
+        input.focus();
+    }
+});
+
+
